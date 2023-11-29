46b1473f4eea2617e58703824137abbf55f61a34
Harry Williams <hcw29@kent.ac.uk>
2023-11-29 18:28:13 +0000

    Basic stuff

--- message_generator.js
+++ message_generator.js
@@ -1,7 +1,7 @@
 let randomizedMessage = "";
-const inspo1 = ["Reach for the stars"];
-const inspo2 = ["Climb every mountain higher"];
-const inspo3 = ["I said reach!"];
+const inspo1 = ["Reach for the stars", "If you want to achieve more", "You are destined for greatness", "To build a wall, first,", "To become who you were meant to be"];
+const inspo2 = ["Climb every mountain higher", "Move the river as it flows", "break the concrete under your feet", "smash the moon into oblivion", "eat your vegetables"];
+const inspo3 = ["I said reach!", "Then you will know.", "That will be your final test.", "Your story is complete."];
 
 function randNumber(arr){
     return Math.floor(Math.random() * arr.length);
