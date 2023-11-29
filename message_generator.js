d48b2569f84bd3caf7c61d6fb605af29803467e5
Harry Williams <hcw29@kent.ac.uk>
2023-11-29 18:22:02 +0000

    Boilerplate

--- message_generator.js
+++ message_generator.js
@@ -1,3 +1,12 @@
-function messageGenerator(){
-    console.log('This is a funny joke');
+let randomizedMessage = "";
+const inspo1 = ["Reach for the stars"];
+const inspo2 = ["Climb every mountain higher"];
+const inspo3 = ["I said reach!"];
+
+function randNumber(arr){
+    return Math.floor(Math.random() * arr.length);
 }
+
+randomizedMessage = inspo1[randNumber(inspo1)] + ", " + inspo2[randNumber(inspo2)] + ", " + inspo3[randNumber(inspo3)];
+
+console.log(randomizedMessage);
